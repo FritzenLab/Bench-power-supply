1136445192f88fbc84166ce5ae33ab861d3760be
Clovis Fritzen <clovisf@gmail.com>
2025-08-08 22:49:42 -0300

    update

--- Display-code/Display-code.ino
+++ Display-code/Display-code.ino
@@ -7,111 +7,151 @@ Using the Xiao SAMD21 microcontroller programmed with Arduino and showing:
 
 Featuring an i2c 16x2 LCD display and 4.5-11.5V variable power supply
 */
+
 #include <Wire.h>
+//#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_PCF8574.h>
+
+// Pin definitions
 #define Temperature A0
-#define Voltage5V A1
-#define Voltage3V3 A2
-#define Voltage12V A3
-#define Current5V A7
-#define Current3V3 A8
-#define led 13
+#define Voltage5V   A1
+#define Voltage3V3  A2
+#define Voltage12V  A3
+#define Current5V   A7
+#define Current3V3  A8
+#define led         13
 
-#define col 16 
-#define lin  2 
-#define addr  0x27 
-LiquidCrystal_I2C lcd(addr,col,lin);
+// LCD parameters
+#define col   16 
+#define lin   2 
+#define addr  0x3F
+//LiquidCrystal_I2C lcd(addr, col, lin);
+LiquidCrystal_PCF8574 lcd(0x3F);
 
-int ledtime= 0;
-int ntctime= 0;
-int lcdtime= 0;
-int analogReadingsTime= 0;
-double tempCelsius= 0.0;
-float vc[5]= 0;
+// Timers and globals
+unsigned long ledtime = 0;
+unsigned long ntctime = 0;
+unsigned long lcdtime = 0;
+unsigned long analogReadingsTime = 0;
+double tempCelsius = 0.0;
+float voltagesAndCurrents[5] = {0};        // Index: 0=5V, 1=3V3, 2=12V, 3=5V current, 4=3V3 current
+static int counter = 0;
+static float averagedValues[5] = {0};
 
-// Parâmetros do termistor
+// Thermistor parameters
 const double beta = 3950.0;
 const double r0 = 10000.0;
 const double t0 = 273.0 + 25.0;
-const double rx = r0 * exp(-beta/t0);
- 
-// Parâmetros do circuito
+const double rx = r0 * exp(-beta / t0);
+
+// Voltage divider and ADC setup
 const double vcc = 4.92;
 const double R = 9810.0;
- 
-// Numero de amostras na leitura
 const int nAmostras = 5;
+
+int error;
 
 void setup() {
   Wire.begin();
-  analogReadResolution(12); // 4095 steps
-  pinMode(13, OUTPUT);
+  Wire.beginTransmission(0x3F);
+  analogReadResolution(12); // 12-bit ADC for SAMD21 (0-4095)
+  pinMode(led, OUTPUT);
 
-  // reference for i2c LCD: https://www.blogdarobotica.com/2022/05/02/como-utilizar-o-display-lcd-16x02-com-modulo-i2c-no-arduino/
-  lcd.init(); 
-  lcd.backlight(); 
-  lcd.clear(); 
+  error = Wire.endTransmission();
+  
 
+  if (error == 0) {
+    
+    int show = 0;
+    lcd.begin(16, 2);  // initialize the lcd
+
+    //lcd.createChar(1, dotOff);
+    //lcd.createChar(2, dotOn);
+
+  } else {
+    
+  }  // if
+
+  //lcd.init(); 
+  //lcd.backlight(); 
+  lcd.setBacklight(127);
+  //lcd.clear(); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   blinkLED();
-  tempCelsius= readTemperature();
-  voltagesAndCurrents= readVoltagesAndCurrents(vc);
+  tempCelsius = readTemperature();
+  readVoltagesAndCurrents(voltagesAndCurrents);
   updateDisplay(tempCelsius, voltagesAndCurrents);
 }
 
-void blinkLED(){
-  if(millis() - ledtime > 300){
-    ledtime= millis();
+void blinkLED() {
+  if (millis() - ledtime > 300) {
+    ledtime = millis();
     digitalWrite(led, !digitalRead(led));
   }
 }
-float readTemperature(){
-  if(millis() - ntctime > 1000){
-    ntctime= millis();
-    // Le o sensor algumas vezes
+
+float readTemperature() {
+  if (millis() - ntctime > 1000) {
+    ntctime = millis();
     int soma = 0;
     for (int i = 0; i < nAmostras; i++) {
       soma += analogRead(Temperature);
-      delay (10);
+      delay(10);
     }
-  
-    // Determina a resistência do termistor
-    double v = (vcc*soma)/(nAmostras*4096.0);
-    double rt = (vcc*R)/v - R;
-  
-    // Calcula a temperatura
-    double t = beta / log(rt/rx);
-    tempCelsius= t-273.0;
+
+    double v = (vcc * soma) / (nAmostras * 4096.0);
+    double rt = (vcc * R) / v - R;
+    double t = beta / log(rt / rx);
+    tempCelsius = t - 273.0;
   }
   return tempCelsius;
 }
 
-void updateDisplay(double tempCelsius, float voltagesAndCurrents){
-  if(millis() - lcdtime > 500){
-    lcdtime= millis();
-    lcd.clear();
-    lcd.setCursor(1,0); // column 1, line 0
-    lcd.print("FritzenLab");
-    lcd.setCursor(1,1); // column 1, line 0
-    lcd.print("tempCelsius");
-  }
-  
-}
-void readVoltagesAndCurrents(float voltagesAndCurrents[5]){
-  if(millis() - analogReadingsTime > 50){
-    analogReadingsTime= millis();
+void readVoltagesAndCurrents(float voltagesAndCurrents[5]) {
+  if (millis() - analogReadingsTime > 50) {
+    analogReadingsTime = millis();
     counter++;
-    if(counter < 4){
-      voltagesAndCurrents[0]= analogRead(voltage5V) * (3.3 / 4096) * 2 + voltagesAndCurrents[0];
-      voltagesAndCurrents[1]= analogRead(voltage3V3) * (3.3 / 4096) * 2 + voltagesAndCurrents[1];
-      voltagesAndCurrents[2]= analogRead(voltage12V) * ((3.3 / 4096) / 0.15) + voltagesAndCurrents[2];
-      voltagesAndCurrents[3]= ((analogRead(current5V) * (5 / 4096)) - 2.5) * 5.4 + voltagesAndCurrents[3];
-      voltagesAndCurrents[4]= ((analogRead(current3V3) * (5 / 4096)) - 2.5) * 5.4 + voltagesAndCurrents[4];
-    }else{
-      counter= 0;
+
+    if (counter < 5) {
+      voltagesAndCurrents[0] += analogRead(Voltage5V) * (3.3 / 4096.0) * 2; // voltage divider x2
+      voltagesAndCurrents[1] += analogRead(Voltage3V3) * (3.3 / 4096.0) * 2;
+      voltagesAndCurrents[2] += analogRead(Voltage12V) * ((3.3 / 4096.0) / 0.15); // assume scaling factor
+      voltagesAndCurrents[3] += ((analogRead(Current5V) * (5.0 / 4096.0)) - 2.5) * 5.4;
+      voltagesAndCurrents[4] += ((analogRead(Current3V3) * (5.0 / 4096.0)) - 2.5) * 5.4;
+    } else {
+      counter = 0;
+      for (int i = 0; i < 5; i++) {
+        averagedValues[i] = voltagesAndCurrents[i] / 4.0;
+        voltagesAndCurrents[i] = 0; // reset accumulation
+      }
     }
   }
-  return voltagesAndCurrents;
+}
+
+void updateDisplay(double tempCelsius, float voltagesAndCurrents[5]) {
+  if (millis() - lcdtime > 1000) {
+    lcdtime = millis();
+    //lcd.clear();
+    /*lcd.setCursor(0, 0);
+    lcd.print("T:");
+    lcd.print(tempCelsius, 1);
+    lcd.print((char)223); // degree symbol
+    lcd.print("C ");
+
+    lcd.print("5V:");
+    lcd.print(averagedValues[0], 1);
+
+    lcd.setCursor(0, 1);
+    lcd.print("3V3:");
+    lcd.print(averagedValues[1], 1);
+
+    lcd.print(" 12V:");
+    lcd.print(averagedValues[2], 1);*/
+    lcd.setCursor(0, 0);
+    lcd.print("*** first line.");
+    lcd.setCursor(0, 1);
+    lcd.print("*** second line.");
+  }
 }
